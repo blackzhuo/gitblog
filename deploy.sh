399c1a612c09a6f7d0c4ad213c12ce9ba657653d
zhuo.wu <zhuo.wu@qunar.com>
2017-02-08 17:51:01 +0800

    添加百度推送

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-hexo clean && hexo g && hexo d
+hexo clean
+hexo g
+hexo d
+
+curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.xuerlove.com&token=qYDK8jfBtWRoSXDs"
