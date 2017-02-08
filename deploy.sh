e0b88325fd48818b4b5f2b0542ea6125aaf241b8
zhuo.wu <zhuo.wu@qunar.com>
2017-02-08 17:52:15 +0800

    fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+# 编译部署
 hexo clean
 hexo g
 hexo d
 
+# 百度seo推送
 curl -H 'Content-Type:text/plain' --data-binary @urls.txt "http://data.zz.baidu.com/urls?site=www.xuerlove.com&token=qYDK8jfBtWRoSXDs"
